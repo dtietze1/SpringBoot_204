c60a15443bc801db2ee4691cacf72c7fe0cdc97a
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-14 18:32:00 -0400

    Uploading Exercise 2.04

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @GetMapping("/tvform")
+    public String loadTvForm(Model mod1){
+        mod1.addAttribute("tvshow", new Tvshow());
+        return "tvform";
+    }
+
+    @PostMapping("/tvform")
+    public String processTvForm(@Valid Tvshow tvshow, BindingResult result){
+        if (result.hasErrors()){
+            return "tvform";
+        }
+        return "tvshowconfirm";
+    }
+}
